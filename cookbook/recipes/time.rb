d362e106038b32643e6484e5f8c7835ee9edb24e
Frank-Robert Kline <frank-robert.kline@hightail.com>
2014-07-06 11:44:01 -0700

    basic common cookbook with ntp

---
+++ cookbook/recipes/time.rb
@@ -0,0 +1,19 @@
+#
+# Cookbook Name:: cb-common
+# Recipe:: time
+#
+# Copyright (C) 2014 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+# Set timezone to UTC
+# http://community.opscode.com/cookbooks/timezone-ii
+node.normal['tz'] = 'UTC'
+include_recipe 'timezone-ii'
+
+# Start NTP with default timeservers for local geography
+# http://community.opscode.com/cookbooks/ntp
+# http://support.ntp.org/bin/view/Servers/NTPPoolServers
+node.normal['ntp']['servers'] = ['0.pool.ntp.org', '1.pool.ntp.org', '2.pool.ntp.org', '3.pool.ntp.org']
+include_recipe 'ntp'
